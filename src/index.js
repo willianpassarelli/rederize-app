30f93cf8de88dcae6c9ed9e4aa73c3da283bb4f8
willianpassarelli <willian.passarelli@hotmail.com>
2020-05-06 11:37:34 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+
+import './config/ReactotronConfig';
+
+import { store, persistor } from './store';
+
+import App from './routes';
+
+export default function Index() {
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="dark-content" />
+          <App />
+        </PersistGate>
+      </Provider>
+    </NavigationContainer>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
-const Routes = async () => {
+export default () => {
   const signed = useSelector(state => state.auth.signed);
+
   return signed ? <AppRoutes /> : <AuthRoutes />;
 };
-
-export default Routes;
